af839bcac2d662d2229cc4f1190016e11bdc1892
m.aantjes <65569213+MaxAantjes@users.noreply.github.com>
2020-08-13 13:55:31 +0200

    Wrote Population Generator

--- Generator.R
+++ Generator.R
@@ -1,2 +1,22 @@
-generator <- function(seed = 1) {
-        set.seed(seed)}
+library(tidyverse)
+
+## Set the seed
+SEED <- 1
+
+## Set the total population size
+TOTPOP <- 1000000
+
+## OPTIONAL: generate the subpopulation size, subpopulation size is assumed to be normally distributed. 
+SUBNUMBER <- 240 
+        
+x <- data.frame(rnorm(SUBNUMBER, 100, 30))
+names(x) <- "value"
+x <- x%>%
+        mutate(tot = sum(value)) %>%
+        mutate(prop = value/tot) %>%
+        mutate(population = prop * SIZE) %>%
+        mutate(test = sum(population))
+
+generator <- function(seed = SEED, size = SIZE) {
+        set.seed(seed)
+        }
